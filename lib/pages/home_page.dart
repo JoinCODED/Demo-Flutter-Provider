2711fe070a283958e15846e4ddcb65d54a083e03
Dhari <dmakwt@gmail.com>
2021-08-21 23:49:15 -0500

    fix the Textfields

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,9 +12,9 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController _bodyTextEditingController =
       TextEditingController();
 
-  // TODO: #1 note list
+  // TODO: #2 note list
 
-  // TODO: #2 add note function
+  // TODO: #3 add note function
 
   @override
   Widget build(BuildContext context) {
@@ -30,48 +30,9 @@ class _HomePageState extends State<HomePage> {
           ),
           backgroundColor: Theme.of(context).primaryColor,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Card(
-                  elevation: 10,
-                  child: ListTile(
-                    title: Column(
-                      children: [
-                        TextField(
-                          controller: _titleTextEditingController,
-                          decoration: InputDecoration(
-                            hintText: 'Title',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                        TextField(
-                          controller: _bodyTextEditingController,
-                          decoration: InputDecoration(
-                            hintText: 'Body',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ],
-                    ),
-                    trailing: IconButton(
-                      iconSize: 32,
-                      icon: Icon(
-                        Icons.add_box,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
-              ),
 
-              // TODO: #3 Add ListView builder
-            ],
-          ),
-        ),
+        // TODO: #1 Textfields
+        body: Container(),
       ),
     );
   }
